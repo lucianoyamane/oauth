cc88d9f6cd8e6a531f02f26991c874e1941e54c6
lucianoyamane <lucianoaugusto@gmail.com>
2018-10-23 20:41:05 +0000

    adicionado persistencia do cliente

--- demoresource/src/main/java/com/luciano/oauth/demoresource/DemoresourceApplication.java
+++ demoresource/src/main/java/com/luciano/oauth/demoresource/DemoresourceApplication.java
@@ -2,9 +2,10 @@ package com.luciano.oauth.demoresource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class DemoresourceApplication {
 
 	public static void main(String[] args) {
